03c283eb985d09d398e855b4ed7498b44b2cb7ab
Keyur-Trivedi13 <141047469+Keyur-Trivedi13@users.noreply.github.com>
2023-08-29 23:35:22 +0530

    Update Rocket.js
    Class file code of Rocket is done for Chandrayaan-3 Mission!

--- Rocket.js
+++ Rocket.js
@@ -1,68 +1,146 @@
 //First we will create Rocket class which will call the methods for rotating the rocket and changing its co-ordinates 
 class Rocket
 {
-    Rocket(current_position,intial_direction)
+    constructor(current_position,intial_direction)
     {
         //we are intialzing rocket coordinates and direction at start time
         this.rocket_position=current_position;
         this.rocket_direction=intial_direction;
+        this.rocket_temp_direction="N";//this direction will be useful In Up And Down operation
     }
     //we will be calling this method when we will receive 'f' word from character array
-    move_Forward()
+    Move_Forward()
     {   
         //set the coordinates of rocket according to current direction
         if(this.rocket_direction=="N")
         {
-            this.rocket_position[1]++;//Y-coordinate--North
+            this.rocket_position[1]++;
         }
-        else if(this.rocket_direction="S")
+        else if(this.rocket_direction=="S")
         {
-            this.rocket_position[1]--;//Y-coordinate--South
+            this.rocket_position[1]--;
         }
         else if(this.rocket_direction=="E")
         {
-            this.rocket_direction[0]++;//X-coordinate--East
+            this.rocket_position[0]++;
         }
         else if(this.rocket_direction=="W")
         {
-            this.rocket_direction[0]++;//X-coordinate--West
+            this.rocket_position[0]--;
         }
-        else if(this.rocket_direction=="Up")
+        else if(this.rocket_direction=="U")
         {
-            this.rocket_direction[2]++;//Z-coordinate--Up
+            this.rocket_position[2]++;
         }   
-        else if(this.rocket_direction=="Down")
+        else if(this.rocket_direction=="D")
         {
-            this.rocket_direction[2]--;//Z-coordinate--Down
+            this.rocket_position[2]--;
         }
     }
     //we will be calling this method when we will receive 'b' word from character array
-    move_backward()
+    Move_Backward()
     {   
         //set the coordinates of rocket according to current direction
         if(this.rocket_direction=="N")
         {
-            this.rocket_position[1]++;//Y-coordinate--South
+            this.rocket_position[1]--;
         }
-        else if(this.rocket_direction="S")
+        else if(this.rocket_direction=="S")
         {
-            this.rocket_position[1]--;//Y-coordinate--North
+            this.rocket_position[1]++;
         }
         else if(this.rocket_direction=="E")
         {
-            this.rocket_direction[0]++;//X-coordinate--West
+            this.rocket_position[0]--;
         }
         else if(this.rocket_direction=="W")
         {
-            this.rocket_direction[0]++;//X-coordinate--East
+            this.rocket_position[0]++;
         }
-        else if(this.rocket_direction=="Up")
+        else if(this.rocket_direction=="U")
         {
-            this.rocket_direction[2]--;//Z-coordinate--down
+            this.rocket_position[2]--;
         }   
-        else if(this.rocket_direction=="Down")
+        else if(this.rocket_direction=="D")
         {
-            this.rocket_direction[2]++;//Z-coordinate--UP
+            this.rocket_position[2]++;
+        }
+    }
+    //we will be calling this method when we will receive 'r' word from character array
+    Turn_Right()
+    {   
+        //set the direction of rocket according to current direction
+        if(this.rocket_direction=="N")
+        {
+            this.rocket_direction="E";
+        }
+        else if(this.rocket_direction=="S")
+        {
+            this.rocket_direction="W";
+        }
+        else if(this.rocket_direction=="E")
+        {
+            this.rocket_direction="S";
+        }
+        else if(this.rocket_direction=="W")
+        {
+            this.rocket_direction="N";
+        }
+        else if(this.rocket_direction=="U")
+        {
+            this.rocket_direction="S";
+        }
+        else if(this.rocket_direction=="D")
+        {
+            this.rocket_direction="N";
+        }
+    }
+    //we will be calling this method when we will receive 'l' word from character array
+    Turn_Left()
+    {   
+        //set the direction of rocket according to current direction
+        if(this.rocket_direction=="N")
+        {
+            this.rocket_direction="W";
+        }
+        else if(this.rocket_direction=="S")
+        {
+            this.rocket_direction="E";
+        }
+        else if(this.rocket_direction=="E")
+        {
+            this.rocket_direction="N";
+        }
+        else if(this.rocket_direction=="W")
+        {
+            this.rocket_direction="S";
+        }
+        else if(this.rocket_direction=="U")
+        {
+            this.rocket_direction="N";
+        }
+        else if(this.rocket_direction=="D")
+        {
+            this.rocket_direction="S";
+        }
+    }
+    //we will be calling this method when we will receive 'u' word from character array
+    Rotate_Up()
+    {
+        if(this.rocket_direction!="U")
+        {
+            this.rocket_temp_direction=this.rocket_direction;
+            this.rocket_direction="U";
+        }
+    }
+    //we will be calling this method when we will receive 'd' word from character array
+    Rotate_Down()
+    {
+        if(this.rocket_direction!="D")
+        {
+            this.rocket_temp_direction=this.rocket_direction;
+            this.rocket_direction="D";
         }
     }
 }
+module.exports= Rocket;
